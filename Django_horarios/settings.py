171ae0852322b47b835a7a1b691a71d951defa46
sebas <sebastyk120@gmail.com>
2023-04-25 16:13:31 -0500

    Finalizacion

--- Django_horarios/settings.py
+++ Django_horarios/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lfr-0nqell1=(6u2y(2r_cbxvbv9uxvd4mffu#exs%f%eg@q%+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.1.52']
+ALLOWED_HOSTS = ['192.168.1.250']
 
 MESSAGES_STORAGE = "django.contrib.messages.storage.cookie.CookieStorage"
 # Application definition
